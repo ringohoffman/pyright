c94949f150b50a85250ce75bff5ce8f0e396fc17
Matthew Hoffman <ringohoffman@users.noreply.github.com>
2026-08-19 00:26:10 -0700

    Update mongo model HKT prototype and rename pyarrow test
    - Add ConcreteUser and update User property signatures in mongo-model prototype
    - Document expected rejections when passing 0-ary/proper types to HKT parameterizations
    - Rename cast_preserves_container.py to pyarrow-cast.py

--- hkt-prototypes/mongo-model-comment-843663432.py
+++ hkt-prototypes/mongo-model-comment-843663432.py
@@ -13,7 +13,10 @@ class ObjectId:
 class User:
     __collection__ = "users"  # This is the "table", or collection in Mongo. Class variable, not instance
 
-    _id: ObjectId  # Every Mongo document has an ID, most often ObjectId, but can be maybe str or something else
+    @property
+    def _id(self) -> ObjectId:
+        ...
+
     username: str  # A random property
 
 
@@ -25,11 +28,18 @@ class MongoModel[ID](Protocol):
         ...
 
 
-class SpecialMongoModel[ID]:  # structural inherence
+class SpecialMongoModel[ID]:
     __collection__: str
 
     @property
     def _id(self) -> ID:
+        ...
+
+class ConcreteUser(MongoModel[ObjectId]):
+    __collection__: str = ""
+
+    @property
+    def _id(self) -> ObjectId:
         ...
 
 
@@ -41,5 +51,16 @@ def find_one_3[MongoModelT[IDT]: MongoModel[IDT]](model_cls: type[MongoModelT[ID
 reveal_type(find_one(SpecialMongoModel, 1), expected_text="SpecialMongoModel[int]")
 reveal_type(find_one_2(SpecialMongoModel, 1), expected_text="SpecialMongoModel[int]")
 
-reveal_type(find_one(User, ObjectId()), expected_text="User")
-
+# Note on User and ConcreteUser rejection:
+# `find_one_2` declares `MongoModelT` as a higher-kinded type constructor (kind * -> *).
+# When matching `model_cls: type[MongoModelT[IDT]]`, `MongoModelT` must be a parameterized
+# generic class constructor (like `SpecialMongoModel`) that can be applied to `IDT`.
+#
+# Even though `User` and `ConcreteUser` structurally conform to `MongoModel[ObjectId]`,
+# they are 0-ary (proper) types (kind *), not type constructors. They cannot be applied
+# as `User[ObjectId]` or `ConcreteUser[ObjectId]`.
+#
+# Representing fixed-ID models like `ConcreteUser` alongside generic constructors would
+# require associated types / type families (e.g. `M._id`) rather than type constructor application.
+reveal_type(find_one_2(User, ObjectId()), expected_text="User")  #  "type[User]" is not a generic type constructor
+reveal_type(find_one_2(ConcreteUser, ObjectId()), expected_text="ConcreteUser")  # "MongoModel[ObjectId]" is not a concrete class type and cannot be assigned to type "type[MongoModel[IDT@find_one_2]]"
